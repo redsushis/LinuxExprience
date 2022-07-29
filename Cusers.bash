8b35c97229dcb822006c5c2e50c265a44429c547
redsushis <110244012+redsushis@users.noreply.github.com>
2022-07-29 11:43:25 -0300

    Add files via upload

--- Cusers.bash
+++ Cusers.bash
@@ -4,31 +4,31 @@
 
 echo "Iniciando criação de Usuarios"
 
-useradd carlos -c "carlos" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd carlos -c "carlos" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd carlos -e
 
-useradd maria -c "maria" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd maria -c "maria" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd maria -e
 
-useradd --force-badname joao_ -c "joao_" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd --force-badname joao_ -c "joao_" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd joao_ -e
 
-useradd debora -c "debora" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd debora -c "debora" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd debora -e
 
-useradd sebastiana -c "sebastiana" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd sebastiana -c "sebastiana" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd sebastiana -e
 
-useradd roberto -c "roberto" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd roberto -c "roberto" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd roberto -e
 
-useradd josefina -c "josefina" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd josefina -c "josefina" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd josefina -e
 
-useradd amanda -c "amanda" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd amanda -c "amanda" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd amanda -e
 
-useradd rogerio -c "rogerio" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd rogerio -c "rogerio" -s /bin/bash -m -p $(openssl passwd -crypt senhaloki)
 passwd rogerio -e
 
 echo "Fim"
